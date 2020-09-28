828919ac2a8c91d99fd354e2eff9f06a80815b71
Alex Eesley <alexandra.eesley@gmail.com>
2020-09-28 15:39:20 -0500

    added css to style the timer section so that is was right justified, a smaller font, and shown in red

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 // JS GOES HERE
-var timeEl = document.querySelector(".time");
-var mainEl = document.getElementById("main");
+var timeEl = document.querySelector(".time"); // defining time class on html doc
+var mainEl = document.getElementById("main"); // defining main id on html doc
+
+var secondsLeft = 75; // defining totall time alloted to user taking quiz
+
+function setTime() {
+    var timerInterval = setInterval(function() {
+        secondsLeft--; //shows that time will decrease
+        timeEl.textContent = secondsLeft + " seconds left!"; // shows message that will display on page with time counting down
+
+        if(secondsLeft === 0) {
+            clearInterval(timerInterval);
+            alert("Time is up! GAME OVER!") // shows that when second countdown reaches zero, a message will show that game is over
+        }
+    }, 1000); // denotes that time will decrease by one second at a time
+}
+
+setTime();

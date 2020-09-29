b63838e30ed617f24956dd01ba64fe03585ed3a0
Alex Eesley <alexandra.eesley@gmail.com>
2020-09-29 15:32:59 -0500

    commented timer for now. Added in new sections on html, new variables, and starting function lines on js for quiz

--- script.js
+++ script.js
@@ -1,22 +1,44 @@
 // JS GOES HERE
 var timeEl = document.querySelector(".time"); // defining time class on html doc
 var mainEl = document.getElementById("main"); // defining main id on html doc
+var quizContainer = document.getElementsById('quiz');
+var resultsContainer = document.getElementById('results');
+var submitButton = document.getElementById('submit');
 
-var secondsLeft = 75; // defining totall time alloted to user taking quiz
 
-function setTime() {
-    var timerInterval = setInterval(function() {
-        secondsLeft--; //shows that time will decrease
-        timeEl.textContent = secondsLeft + " seconds left!"; // shows message that will display on page with time counting down
+function buildQuiz(){
 
-        if(secondsLeft === 0) {
-            clearInterval(timerInterval);
-            alert("Time is up! GAME OVER!") // shows that when second countdown reaches zero, a message will show that game is over
-        }
-    }, 1000); // denotes that time will decrease by one second at a time
+}
+
+function showResults(){
+
+}
+
+
+buildQuiz(); //display quiz right away
+
+submitButton.addEventListener('click', showResults);
+
+
+
+
+
+// var secondsLeft = 75; // defining totall time alloted to user taking quiz
+
+// function setTime() {
+//     var timerInterval = setInterval(function() {
+//         secondsLeft--; //shows that time will decrease
+//         timeEl.textContent = secondsLeft + " seconds left!"; // shows message that will display on page with time counting down
+
+//         if(secondsLeft === 0) {
+//             clearInterval(timerInterval);
+//             alert("Time is up! GAME OVER!") // shows that when second countdown reaches zero, a message will show that game is over
+//         }
+//     }, 1000); // denotes that time will decrease by one second at a time
 
 
     
-}
+// }
 
-setTime(); // this is calling the function so the timer actually runs
+// setTime(); // this is calling the function so the timer actually runs
+
